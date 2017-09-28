628df1b3dd5c454f77b2a386b01142285eed738a
Boma <pbomestar@gmail.com>
2017-09-28 22:40:18 +0200

    dugmadi test

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(480, 640, Phaser.CANVAS, null, {preload: preload, create: create, update: update});
 
-var startButton;
+var startButton, exitButton, prevButton, nextButton;
 
 
 function preload() {
@@ -8,14 +8,28 @@ function preload() {
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = '#eee';
-    game.load.spritesheet('startBtn', 'img/start12.png', 200, 35);
+    game.load.spritesheet('startBtn', 'img/start12.png', 100, 35);
+    game.load.spritesheet('exitBtn', 'img/exit12.png', 150, 50);
+    game.load.spritesheet('prevnextBtn', 'img/prevnext.png', 70, 50);
 }
 function create() {
     startButton = game.add.button(game.world.width*0.5, game.world.height*0.3, 'startBtn', startGame, this, 0, 0, 1, 2);
     startButton.anchor.set(0.5);
+    exitButton = game.add.button(game.world.width*0.5, game.world.height*0.5, 'exitBtn', exitGame, this, 0, 0, 1, 2);
+    exitButton.anchor.set(0.5);
+	prevButton = game.add.button(game.world.width*0.3, game.world.height*0.7, 'prevnextBtn', prevGame, this, 0, 0, 1, 2);
+    prevButton.anchor.set(0.5);
+	nextButton = game.add.button(game.world.width*0.7, game.world.height*0.7, 'prevnextBtn', nextGame, this, 0, 0, 2, 1);
+    nextButton.anchor.set(0.5);
 }
 function update() {}
 
 function startGame(){
 	startButton.kill();
+	exitButton.kill();
+	prevButton.kill();
+	nextButton.kill();
 }
+function exitGame(){}
+function prevGame(){}
+function nextGame(){}
