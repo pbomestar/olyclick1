3ec5ecd921fb85b6d7554cc84f7366df238a3860
Boma <pbomestar@gmail.com>
2017-09-28 21:35:31 +0200

    smth

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -1,10 +1,21 @@
 var game = new Phaser.Game(480, 640, Phaser.CANVAS, null, {preload: preload, create: create, update: update});
 
+var startButton;
+
+
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = '#eee';
+    game.load.spritesheet('startBtn', 'img/start12.png', 200, 35);
 }
-function create() {}
+function create() {
+    startButton = game.add.button(game.world.width*0.5, game.world.height*0.3, 'startBtn', startGame, this, 0, 0, 1, 2);
+    startButton.anchor.set(0.5);
+}
 function update() {}
+
+function startGame(){
+	startButton.kill();
+}
